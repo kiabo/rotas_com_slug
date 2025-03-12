20b450705cde4c9e31701adf9d3a744a0f36ca20
Fabiano S Figueiredo <fabiano@comoqueta.la>
2025-03-12 11:16:50 -0300

    add navigation links and routes for users and cities

---
+++ resources/views/users.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <section class="p-6">
+        <div class="max-w-4xl mx-auto">
+            <h3 class="text-xl">Usuários</h3>
+
+            <ul>
+                <li>Usuário 1</li>
+                <li>Usuário 2</li>
+                <li>Usuário 3</li>
+                <li>Usuário 4</li>
+            </ul>
+        </div>
+    </section>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/users', function () {
+    return view('users');
+})->name('users');
+
+Route::get('/sao-paulo', function () {
+    return view('sao-paulo');
+})->name('sao-paulo');
 
 Route::prefix('{city:slug}')->group(function () {
     Route::get('/', [ProductController::class, 'index'])->name('products.index');
     Route::get('/{product:slug}', [ProductController::class, 'show'])->name('products.show');
 });
+
+Route::get('/belo-horizonte', function () {
+    return 'belo-horizonte';
+})->name('belo-horizonte');
+
+
