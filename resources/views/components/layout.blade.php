19cebfb3397123f1f9a4e15d674c6a59a31dd699
Fabiano S Figueiredo <fabiano@comoqueta.la>
2025-03-12 11:18:10 -0300

    add title to layout for slug route testing

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
 <head>
+    <title>Teste com rotas com slug</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite('resources/css/app.css')
